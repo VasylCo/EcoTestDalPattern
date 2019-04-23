cdfb2935456c4b8d55a6e3a7bae430f660223064
VasylFo <vasyl.fomichov@outlook.com>
2019-04-23 16:49:39 +0300

    Add project files.

---
+++ EcoTestDalPattern/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EcoTestDalPattern.Startup))]
+namespace EcoTestDalPattern
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
